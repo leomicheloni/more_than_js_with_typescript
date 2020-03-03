206663398b1fce8b2214a6fd50af4f49987ca441
Leonardo Micheloni <leomicheloni@hotmail.com>
2020-03-03 22:49:30 +0100

    classes

--- greeter.js
+++ greeter.js
@@ -1,5 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Jane", lastName: "User" };
-document.body.textContent = greeter(user);
+var user = new Student("Jane", "M.", "User");
+console.log(greeter(user));
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -7,7 +14,6 @@ function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-let user = { firstName: "leonardo", lastName: "micheloni" };
+let user = new Student("Jane", "M.", "User");
 
-console.log( greeter(user) );
-
+console.log(greeter(user));
